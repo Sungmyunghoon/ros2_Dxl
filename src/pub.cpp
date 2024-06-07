eca401b08280f4f98f1543fbb9dc0efaa5fea3b4
Sungmyunghoon <112747810+Sungmyunghoon@users.noreply.github.com>
2024-06-07 11:17:23 +0900

    Update pub.cpp

--- src/pub.cpp
+++ src/pub.cpp
@@ -1,10 +1,12 @@
+// dxl/pub.hpp와 dxl/dxl.hpp 헤더 파일을 포함
 #include "dxl/pub.hpp"
 #include "dxl/dxl.hpp"
 
-DxlPublisher::DxlPublisher()
-    : Node("node_dxlpub"), vel1_(0), vel2_(0), goal1_(0), goal2_(0)
+DxlPublisher::DxlPublisher() // 클래스의 생성자
+    : Node("node_dxlpub"), vel1_(0), vel2_(0), goal1_(0), goal2_(0) // 노드의 클래스를 초기화하여 노드 이름을 "node_dxlpub"로 설정
 {
     publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("topic_dxlpub", rclcpp::QoS(rclcpp::KeepLast(10)));
+    // 속도 변수(vel1_, vel2_)와 목표 속도(goal1_, goal2_)를 0으로 초기화
     vel_.x = 0;
     vel_.y = 0;
     vel_.z = 0;
@@ -12,7 +14,7 @@ DxlPublisher::DxlPublisher()
 
 void DxlPublisher::publish_velocity()
 {
-    rclcpp::WallRate loop_rate(20.0); // 20Hz -> 50ms
+    rclcpp::WallRate loop_rate(20.0); // 20Hz -> 50ms publish_velocity 함수는 20Hz(50ms) 주기로 실행
 
     while (rclcpp::ok())
     {
