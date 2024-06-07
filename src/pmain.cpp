e29cfcacd4982479cd337083d2edab13aefd55de
Sungmyunghoon <112747810+Sungmyunghoon@users.noreply.github.com>
2024-06-07 11:15:00 +0900

    Update pmain.cpp

--- src/pmain.cpp
+++ src/pmain.cpp
@@ -1,11 +1,11 @@
-#include "dxl/pub.hpp"
-#include <memory>
+#include "dxl/pub.hpp" // DxlPublisher 클래스를 포함하기 위한 헤더 파일
+#include <memory> // 스마트 포인터 사용을 위한 헤더 파일
 
 int main(int argc, char* argv[])
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<DxlPublisher>();
-    node->publish_velocity();
+    rclcpp::init(argc, argv); // ROS2 시스템을 초기화
+    auto node = std::make_shared<DxlPublisher>(); // 클래스의 인스턴스를 생성
+    node->publish_velocity(); // 노드의 메서드를 호출
     rclcpp::shutdown();
     return 0;
 }
